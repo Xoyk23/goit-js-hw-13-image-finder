34489403f7b136e3bb863b9b49a2cc560c8c460b
Ihor Prokopets <kiringa23@gmail.com>
2021-01-05 13:54:14 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './styles.css';
+import 'material-design-icons/iconfont/material-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/js/bootstrap.esm.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
